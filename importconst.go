c8da4a58e6a21c1ec3c2731c63266f87fff0c027
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-06-23 00:57:44 +0900

    support NAME:TYPE:FORMAT parameter

--- importconst.go
+++ importconst.go
@@ -19,6 +19,8 @@ var headers []string
 var stdheaders = []string{"<stdio.h>", "<windows.h>"}
 var names = []string{}
 
+var macros = map[string][2]string{}
+
 func parse() bool {
 	for _, arg1 := range os.Args[1:] {
 		if arg1 == "-d" {
@@ -31,10 +33,14 @@ func parse() bool {
 			headers = append(headers, arg1)
 		} else if strings.HasSuffix(arg1, ".h>") {
 			stdheaders = append(stdheaders, arg1)
+		} else if p := strings.Split(arg1, ":"); len(p) == 3 {
+			macros[p[0]] = [2]string{p[1], p[2]}
 		} else if packagename == "" {
 			packagename = arg1
-		} else {
+		} else if strings.ContainsRune(arg1, '(') {
 			names = append(names, arg1)
+		} else {
+			macros[arg1] = [2]string{"", "%d"}
 		}
 	}
 	return true
@@ -59,6 +65,15 @@ func make_csource(csrcname string) {
 	fmt.Fprintln(fd, `#define s(n) printf("const " #n "=\"%s\"\n",n)`)
 	fmt.Fprintln(fd, `#define u32x(n) printf("const " #n "=uint32(0x%08X)\n",n)`)
 	fmt.Fprintln(fd, `#define up(n) printf("const " #n "=uintptr(%d)\n",n)`)
+	for key, val := range macros {
+		fmt.Fprintf(fd, `#define MAKECONST_%s(n) printf("const " #n "=`, key)
+		if val[0] != "" {
+			fmt.Fprintf(fd, `%s(%s)`, val[0], val[1])
+		} else {
+			fmt.Fprintf(fd, "%s", val[1])
+		}
+		fmt.Fprintln(fd, `\n",n)`)
+	}
 	fmt.Fprintln(fd, ``)
 	fmt.Fprintln(fd, `int main()`)
 	fmt.Fprintln(fd, `{`)
@@ -66,6 +81,9 @@ func make_csource(csrcname string) {
 
 	for _, name1 := range names {
 		fmt.Fprintf(fd, "    %s;\n", name1)
+	}
+	for name1, _ := range macros {
+		fmt.Fprintf(fd, "    MAKECONST_%s(%s);\n", name1, name1)
 	}
 	fmt.Fprintln(fd, "    return 0;\n}\n")
 }

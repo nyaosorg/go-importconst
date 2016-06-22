001cf420bc86071a66e3d47c0a30b893c24f6f95
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-06-23 07:18:19 +0900

    Add example\makeconst.cmd

---
+++ example/makeconst.cmd
@@ -0,0 +1,26 @@
+..\go-importconst dos ^
+    d(FILE_ATTRIBUTE_NORMAL) ^
+    d(FILE_ATTRIBUTE_REPARSE_POINT) ^
+    d(FILE_ATTRIBUTE_HIDDEN) ^
+    d(CP_THREAD_ACP) ^
+    d(MOVEFILE_REPLACE_EXISTING) ^
+    d(MOVEFILE_COPY_ALLOWED) ^
+    d(MOVEFILE_WRITE_THROUGH) ^
+    d(SW_HIDE) ^
+    d(SW_MAXIMIZE) ^
+    d(SW_MINIMIZE) ^
+    d(SW_RESTORE) ^
+    SW_SHOW ^
+    SW_SHOWDEFAULT ^
+    SW_SHOWMAXIMIZED ^
+    SW_SHOWMINIMIZED ^
+    SW_SHOWMINNOACTIVE ^
+    SW_SHOWNA ^
+    SW_SHOWNOACTIVATE ^
+    SW_SHOWNORMAL ^
+    COINIT_APARTMENTTHREADED:uintptr:0x%%x ^
+    COINIT_MULTITHREADED:uintptr:0x%%x ^
+    COINIT_DISABLE_OLE1DDE:uintptr:0x%%x ^
+    COINIT_SPEED_OVER_MEMORY:uintptr:0x%%x
+@echo off
+rem go-importconst -> https://github.com/zetamatta/go-importconst

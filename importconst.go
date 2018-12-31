686798ec9e88bd2262241632fa2fca11e1a5478b
HAYAMA_Kaoru <iyahaya@nifty.com>
2018-12-31 17:30:36 +0900

    It is no neccesary to specify type of constants now

--- importconst.go
+++ importconst.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -8,7 +9,7 @@ import (
 )
 
 // CSource is the filename of the temporary file *.c
-const CSource = "makeconst.c"
+const CSource = "makeconst.cpp"
 
 // AExe is the filename of the temporary file *.exe
 const AExe = "a.exe"
@@ -19,84 +20,45 @@ const CC = "gcc.exe"
 // GoSource is the filename of the temporary file *.go
 const GoSource = "const.go"
 
-var debug = false
+var clean = flag.Bool("c", false, "clean output")
+var debug = flag.Bool("d", false, "debug flag")
+var packageName = flag.String("p", "main", "package name")
 
-var packagename string
-var headers []string
-var stdheaders = []string{"<stdio.h>", "<windows.h>"}
-var names = []string{}
-
-var macros = map[string][2]string{}
-
-func parse() bool {
-	for _, arg1 := range os.Args[1:] {
-		if arg1 == "-d" {
-			debug = true
-		} else if arg1 == "-c" {
-			os.Remove(CSource)
-			os.Remove(GoSource)
-			return false
-		} else if strings.HasSuffix(arg1, ".h") {
-			headers = append(headers, arg1)
-		} else if strings.HasSuffix(arg1, ".h>") {
-			stdheaders = append(stdheaders, arg1)
-		} else if p := strings.Split(arg1, ":"); len(p) >= 2 {
-			if len(p) == 2 {
-				p = append(p, "%d")
-			}
-			macros[p[0]] = [2]string{p[1], p[2]}
-			names = append(names, p[0])
-		} else if packagename == "" {
-			packagename = arg1
-		} else if strings.ContainsRune(arg1, '(') {
-			names = append(names, arg1)
-		} else {
-			macros[arg1] = [2]string{"", "%d"}
-			names = append(names, arg1) // for keep argment order.
-		}
-	}
-	return true
-}
-
-func makeCSource(csrcname string, stdheaders []string, header []string) {
+func makeCSource(csrcname string, headers []string, vars []string) {
 	fd, err := os.Create(csrcname)
 	if err != nil {
-		fmt.Fprintf(fd, "%s: can not create makeconst.c\n", os.Args[0])
+		fmt.Fprintf(fd, "%s: can not create %s\n", os.Args[0], csrcname)
 		return
 	}
 	defer fd.Close()
 
-	for _, header1 := range stdheaders {
+	for _, header1 := range headers {
 		fmt.Fprintf(fd, "#include %s\n", header1)
 	}
-	for _, header1 := range headers {
-		fmt.Fprintf(fd, "#include \"%s\"\n", header1)
-	}
-	fmt.Fprintln(fd, ``)
-	fmt.Fprintln(fd, `#define d(n) printf("const " #n "=%d\n",n)`)
-	fmt.Fprintln(fd, `#define s(n) printf("const " #n "=\"%s\"\n",n)`)
-	for key, val := range macros {
-		fmt.Fprintf(fd, `#define MAKECONST_%s(n) printf("const " #n "=`, key)
+	fmt.Fprint(fd, `
+void p(const char *name,const char *s){
+	printf("const %s=\"%s\"\n",name,s);
+}
+void p(const char *name,int n){
+	printf("const %s=%d\n",name,n);
+}
+void p(const char *name,long n){
+	printf("const %s=%d\n",name,n);
+}
+void p(const char *name,unsigned long n){
+	printf("const %s=%d\n",name,n);
+}
+void p(const char *name,double n){
+	printf("const %s=%lf\n",name,n);
+}
 
-		format := strings.Replace(val[1], `"`, `\"`, -1)
-		if val[0] != "" {
-			fmt.Fprintf(fd, `%s(%s)`, val[0], format)
-		} else {
-			fmt.Fprintf(fd, "%s", format)
-		}
-		fmt.Fprintln(fd, `\n",n)`)
-	}
-	fmt.Fprintln(fd, ``)
-	fmt.Fprintln(fd, `int main()`)
-	fmt.Fprintln(fd, `{`)
-	fmt.Fprintln(fd, `    printf("package `+packagename+`\n\n");`)
+int main()
+{
+`)
+	fmt.Fprintln(fd, `    printf("package `+*packageName+`\n\n");`)
 
-	for _, name1 := range names {
-		if _, ok := macros[name1]; ok {
-			fmt.Fprintf(fd, "    MAKECONST_%s(%s);\n", name1, name1)
-		} else {
-			fmt.Fprintf(fd, "    %s;\n", name1)
-		}
+	for _, name1 := range vars {
+		fmt.Fprintf(fd, "    p(\"%s\",%s);\n", name1, name1)
 	}
 	fmt.Fprintln(fd, "    return 0;\n}\n")
 }
@@ -160,31 +122,34 @@ func main1() error {
 }
 
 func main() {
-	if !parse() {
+	flag.Parse()
+
+	if *clean {
+		os.Remove(CSource)
+		os.Remove(AExe)
+		os.Remove(GoSource)
 		return
-	}
-	if len(names) <= 0 {
-		fmt.Fprintf(os.Stderr, "Usage: %s PACKAGE NAME:TYPE:FORMAT...\n", os.Args[0])
-		fmt.Fprintln(os.Stderr, "  -d ... do not remove temporary file")
-		fmt.Fprintln(os.Stderr, "  -c ... clean output-files")
-		fmt.Fprintln(os.Stderr, `  <header.h> \"header.h\" ... append headers`)
-		fmt.Fprintln(os.Stderr, "  NAME             -> const NAME=%d")
-		fmt.Fprintln(os.Stderr, "  NAME:TYPE        -> const NAME=TYPE(%d)")
-		fmt.Fprintln(os.Stderr, "  NAME:TYPE:FORMAT -> const NAME=TYPE(FORMAT)")
-		fmt.Fprintln(os.Stderr, "creates these files.")
-		fmt.Fprintln(os.Stderr, "   -> ./makeconst.c (temporary)")
-		fmt.Fprintln(os.Stderr, "   -> ./a.exe (temporary)")
-		fmt.Fprintln(os.Stderr, "   -> ./const.go")
-		fmt.Fprintln(os.Stderr, "gcc and go-fmt are required.")
-		return
-	}
-	makeCSource(CSource, stdheaders, headers)
-	if !debug {
-		defer os.Remove(CSource)
 	}
 
+	headers := []string{"<windows.h>", "<cstdio>"}
+	vars := make([]string, 0)
+
+	for _, arg1 := range flag.Args() {
+		if len(arg1) > 0 && arg1[0] == '<' {
+			headers = append(headers, arg1)
+		} else if strings.HasSuffix(arg1, ".h") {
+			headers = append(headers, fmt.Sprintf(`"%s"`, arg1))
+		} else {
+			vars = append(vars, arg1)
+		}
+	}
+	makeCSource(CSource, headers, vars)
+
+	if !*debug {
+		defer os.Remove(CSource)
+	}
 	if err := main1(); err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
+		fmt.Fprintln(os.Stderr, err)
 		return
 	}
 }
--- make.cmd
+++ make.cmd
@@ -6,35 +6,34 @@
 @exit /b
 
 :""
-    go fmt importconst.go
+    go fmt
     go build
     exit /b
 
-:"test-null"
-    go-importconst -d "stddef.h" "<stdlib.h>" main d(NULL)
-    exit /b
-
 :"test-dos"
-    go-importconst dos ^
-        d(FILE_ATTRIBUTE_NORMAL) ^
-        d(FILE_ATTRIBUTE_REPARSE_POINT) ^
-        d(FILE_ATTRIBUTE_HIDDEN) ^
-        d(CP_THREAD_ACP) ^
-        d(MOVEFILE_REPLACE_EXISTING) ^
-        d(MOVEFILE_COPY_ALLOWED) ^
-        d(MOVEFILE_WRITE_THROUGH)
+    go-importconst -d -p dos ^
+        FILE_ATTRIBUTE_NORMAL ^
+        FILE_ATTRIBUTE_REPARSE_POINT ^
+        FILE_ATTRIBUTE_HIDDEN ^
+        CP_THREAD_ACP ^
+        MOVEFILE_REPLACE_EXISTING ^
+        MOVEFILE_COPY_ALLOWED ^
+        MOVEFILE_WRITE_THROUGH
     exit /b
 :"test-conio"
     go-importconst ^
-        conio ^
-        d(CTRL_CLOSE_EVENT) ^
-        d(CTRL_LOGOFF_EVENT) ^
-        d(CTRL_SHUTDOWN_EVENT) ^
-        d(CTRL_C_EVENT) ^
-        d(ENABLE_ECHO_INPUT) ^
-        d(ENABLE_PROCESSED_INPUT) ^
-        u32x(STD_INPUT_HANDLE) ^
-        u32x(STD_OUTPUT_HANDLE)
+        -d -p dos conio.h ^
+        CTRL_CLOSE_EVENT ^
+        CTRL_LOGOFF_EVENT ^
+        CTRL_SHUTDOWN_EVENT ^
+        CTRL_C_EVENT ^
+        ENABLE_ECHO_INPUT ^
+        ENABLE_PROCESSED_INPUT ^
+        STD_INPUT_HANDLE ^
+        STD_OUTPUT_HANDLE
+    exit /b
+:"test"
+    go-importconst sample.h AHAHA IHIHI UFUFU
     exit /b
 :"clean"
     go-importconst -c

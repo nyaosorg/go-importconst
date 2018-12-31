6436b4520313bc1db3eb30731d2ad6f3f4774ba9
HAYAMA_Kaoru <iyahaya@nifty.com>
2018-12-31 16:11:30 +0900

    make.cmd: improve

--- make.cmd
+++ make.cmd
@@ -1,15 +1,20 @@
-@echo off
-setlocal
-set GOARCH=386
+@setlocal
+@set GOARCH=386
+@set "PROMPT=MAKE>"
+@call :"%1"
+@endlocal
+@exit /b
 
-if not "%1" == "" goto %1
+:""
     go fmt importconst.go
     go build
-    goto end
-:test-null
+    exit /b
+
+:"test-null"
     go-importconst -d "stddef.h" "<stdlib.h>" main d(NULL)
-    goto end
-:test-dos
+    exit /b
+
+:"test-dos"
     go-importconst dos ^
         d(FILE_ATTRIBUTE_NORMAL) ^
         d(FILE_ATTRIBUTE_REPARSE_POINT) ^
@@ -18,8 +23,8 @@ if not "%1" == "" goto %1
         d(MOVEFILE_REPLACE_EXISTING) ^
         d(MOVEFILE_COPY_ALLOWED) ^
         d(MOVEFILE_WRITE_THROUGH)
-    goto end
-:test-conio
+    exit /b
+:"test-conio"
     go-importconst ^
         conio ^
         d(CTRL_CLOSE_EVENT) ^
@@ -30,10 +35,7 @@ if not "%1" == "" goto %1
         d(ENABLE_PROCESSED_INPUT) ^
         u32x(STD_INPUT_HANDLE) ^
         u32x(STD_OUTPUT_HANDLE)
-    goto end
-:clean
+    exit /b
+:"clean"
     go-importconst -c
-    goto end
-:end
-
-endlocal
+    exit /b
